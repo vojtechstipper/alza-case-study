258d76da6bd5ed66b4332ef0d1ef6ffa6daefcdb
Vojtěch Stipper <vojtech.stipper@gmail.com>
2024-11-06 21:03:47 +0100

    added db model for products added dbcontext created configuration for product

---
+++ AlzaProductAPI/AlzaProduct.Infrastructure/AlzaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AlzaProduct.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlzaProduct.Infrastructure;
+
+public class AlzaDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AlzaDbContext).Assembly);
+    }
+}
---
+++ AlzaProductAPI/AlzaProduct.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AlzaProduct.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlzaProduct.Infrastructure.Persistence.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(a => a.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.ImgUri)
+             .IsRequired()
+             .HasMaxLength(200);
+
+        builder.Property(a => a.Price)
+            .HasPrecision(18, 2);
+    }
+}
