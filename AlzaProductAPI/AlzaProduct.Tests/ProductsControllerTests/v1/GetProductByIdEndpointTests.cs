1c803333ccbc37c58032b78f5d6a67dd75c7e2a6
Vojtěch Stipper <vojtech.stipper@gmail.com>
2024-11-08 22:32:46 +0100

    added tests for endpoints

---
+++ AlzaProductAPI/AlzaProduct.Tests/ProductsControllerTests/v1/GetProductByIdEndpointTests.cs
@@ -0,0 +1,57 @@
+﻿using AlzaProduct.Abstractions;
+using AlzaProduct.Application.Products.DTOs;
+using AlzaProduct.Application.Products.Queries;
+using AlzaProduct.Domain.Entities.Products;
+using AlzaProduct.Infrastructure.Exceptions;
+using AlzaProductAPI.Controllers.v1;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace AlzaProduct.Tests.ProductsControllerTests.v1;
+
+public class GetProductByIdEndpointTests
+{
+    private readonly Mock<IMediator> _mockMediator;
+    private readonly Mock<IRepository<Product>> _productRepository;
+    private readonly Mock<IMapper> _mapper;
+    private readonly ProductsController _controller;
+
+    public GetProductByIdEndpointTests()
+    {
+
+        _mockMediator = new Mock<IMediator>();
+        _controller = new ProductsController(_mockMediator.Object);
+        _productRepository = new Mock<IRepository<Product>>();
+        _mapper = new Mock<IMapper>();
+    }
+
+    [Fact]
+    public async Task GetProductById_ProductExists_ReturnsOkResult()
+    {
+        var product = new Product { ImgUri = "", Name = "Test Product", Price = 10.0M };
+        var productId = product.Id;
+        _mockMediator
+            .Setup(m => m.Send(It.Is<GetProductByIdQuery>(q => q.Id == productId), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ProductDto { Id = productId, ImgUri = product.ImgUri, Name = product.Name, Price = product.Price });
+
+        var result = await _controller.GetProductById(new GetProductByIdQuery(productId));
+
+        var returnedProduct = Assert.IsType<OkObjectResult>(result.Result);
+        var returnedProductProduct = Assert.IsType<ProductDto>(returnedProduct.Value);
+        Assert.Equal(productId, returnedProductProduct.Id);
+        Assert.Equal(200, returnedProduct.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetProductById_ProductDoesNotExist_ReturnsNotFound()
+    {
+        var nonExistingId = "1";
+        _productRepository.Setup(p => p.GetByIdAsync(nonExistingId)).ReturnsAsync((Product?)null);
+
+        var handler = new GetProductByIdQueryHandler(_productRepository.Object, _mapper.Object);
+
+        await Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(new GetProductByIdQuery(nonExistingId), default));
+    }
+}
---
+++ AlzaProductAPI/AlzaProduct.Tests/ProductsControllerTests/v2/GetProductsV2EndpointTests.cs
@@ -0,0 +1,103 @@
+﻿using AlzaProduct.Abstractions;
+using AlzaProduct.Application.Products.DTOs;
+using AlzaProduct.Application.Products.Queries;
+using AlzaProduct.Application.Products.Queries.Validators;
+using AlzaProduct.Domain.Entities.Products;
+using AlzaProductAPI.Controllers.v2;
+using AutoMapper;
+using FluentValidation.TestHelper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace AlzaProduct.Tests.ProductsControllerTests.v1;
+
+public class GetProductsV2EndpointTests
+{
+    private readonly Mock<IMediator> _mockMediator;
+    private readonly Mock<IRepository<Product>> _productRepository;
+    private readonly Mock<IMapper> _mapper;
+    private readonly ProductsController _controller;
+    private readonly GetPaginatedProductsQueryValidator _validator;
+
+    public GetProductsV2EndpointTests()
+    {
+
+        _mockMediator = new Mock<IMediator>();
+        _controller = new ProductsController(_mockMediator.Object);
+        _productRepository = new Mock<IRepository<Product>>();
+        _mapper = new Mock<IMapper>();
+        _validator = new GetPaginatedProductsQueryValidator();
+
+    }
+
+    [Fact]
+    public async Task GetAllProducts_ProductsExists_ReturnsOkResult()
+    {
+        var product = new Product { ImgUri = "", Name = "Test Product", Price = 10.0M };
+        var productId = product.Id;
+
+        _mockMediator
+            .Setup(m => m.Send(It.IsAny<GetPaginatedProductsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new PaginatedList<ProductDto>()
+            {
+                Items = new List<ProductDto> {
+                    new ProductDto {
+                        Id = productId,
+                        ImgUri = product.ImgUri,
+                        Name = product.Name,
+                        Price = product.Price
+                    }
+                }
+            });
+
+        var result = await _controller.GetProductsPaginated(new GetPaginatedProductsQuery());
+
+        var okObjectResult = Assert.IsType<OkObjectResult>(result.Result);
+        var paginatedList = Assert.IsType<PaginatedList<ProductDto>>(okObjectResult.Value);
+        Assert.Equal(productId, paginatedList.Items.First().Id);
+        Assert.Equal(200, okObjectResult.StatusCode);
+    }
+
+    [Fact]
+    public void GetAllProducts_Negative_PageIndex()
+    {
+        var getProductsQuery = new GetPaginatedProductsQuery { PageIndex = -1 };
+
+        var result = _validator.TestValidate(getProductsQuery);
+
+        result.ShouldHaveValidationErrorFor(x => x.PageIndex);
+    }
+
+    [Fact]
+    public Task GetAllProducts_Valid_PageIndex()
+    {
+        var getProductsQuery = new GetPaginatedProductsQuery { PageIndex = 1 };
+
+        var result = _validator.TestValidate(getProductsQuery);
+
+        Assert.True(result.IsValid);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public void GetAllProducts_Negative_PageSize()
+    {
+        var getProductsQuery = new GetPaginatedProductsQuery { PageSize = -1 };
+
+        var result = _validator.TestValidate(getProductsQuery);
+
+        result.ShouldHaveValidationErrorFor(x => x.PageSize);
+    }
+
+    [Fact]
+    public Task GetAllProducts_Valid_PageSize()
+    {
+        var getProductsQuery = new GetPaginatedProductsQuery { PageSize = 10 };
+
+        var result = _validator.TestValidate(getProductsQuery);
+
+        Assert.True(result.IsValid);
+        return Task.CompletedTask;
+    }
+}
