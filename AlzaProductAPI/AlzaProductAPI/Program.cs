29fc49515f1882389605231e77aabf9c36be00d0
Vojtěch Stipper <vojtech.stipper@gmail.com>
2024-11-08 09:10:14 +0100

    added swagger

--- AlzaProductAPI/AlzaProductAPI/Program.cs
+++ AlzaProductAPI/AlzaProductAPI/Program.cs
@@ -7,10 +7,21 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddRepositories();
 builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(options =>
+{
+    options.CustomSchemaIds(x => x.FullName);
+});
 
 var app = builder.Build();
 
 app.MigrateDatabase();
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
