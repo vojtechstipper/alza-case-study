4e80b09d289cbe5c80c6c4f45ca798a99965bc85
Vojtěch Stipper <vojtech.stipper@gmail.com>
2024-11-10 13:16:01 +0100

    changed addAsync to add for better performance

--- AlzaProductAPI/AlzaProduct.Abstractions/IRepository.cs
+++ AlzaProductAPI/AlzaProduct.Abstractions/IRepository.cs
@@ -4,7 +4,7 @@ public interface IRepository<T> where T : Entity
 {
     Task<IEnumerable<T>> GetAllAsync();
     Task<T?> GetByIdAsync(string id);
-    Task AddAsync(T entity);
+    void Add(T entity);
     void Update(T entity);
     void Delete(T entity);
     Task SaveAsync();
